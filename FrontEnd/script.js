e5f5dc643191edee39c641cd63198fc9f821e377
Marion Penel <marionpenel@hotmail.fr>
2025-12-09 16:07:18 +0100

    Création et insertion des éléments du DOM pour chaque projet

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,21 @@
 // Appel de l'API pour récupérer les projets
 async function recupererProjets() {
     const response = await fetch("http://localhost:5678/api/works")
-
     const projets = await response.json();
     console.log("Projets récupérés!", projets);
+    // Création et insertion des éléments du DOM pour chaque projet
+     for (let i=0; i < projets.length; i++) {
+        const ficheProjet = document.createElement("figure");
+        const imageProjet = document.createElement("img");
+        imageProjet.src = projets[i].imageUrl;
+        const titreProjet = document.createElement("figcaption");
+        titreProjet.innerText = projets[i].title;
+
+        ficheProjet.appendChild(imageProjet, titreProjet);
+        document.querySelector(".gallery").appendChild(ficheProjet);
+        
+        console.log("Projet ajouté!", imageProjet, titreProjet);
+    }
 }
 
 recupererProjets();
