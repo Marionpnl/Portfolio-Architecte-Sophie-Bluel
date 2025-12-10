c0bb7c312ff9e6d91bc5a251393d6cd8d45ed07d
Marion Penel <marionpenel@hotmail.fr>
2025-12-10 10:40:15 +0100

    Dissociation fonctions récupération et affichage + début fonctionnalité des filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,9 +1,17 @@
+let projets = []; // Tableau pour stocker les projets récupérés
+let categories = []; // Tableau pour stocker les catégories récupérées
+
 // Appel de l'API pour récupérer les projets
 async function recupererProjets() {
     const response = await fetch("http://localhost:5678/api/works")
-    const projets = await response.json();
+    projets = await response.json(); //liste des projets
+
     console.log("Projets récupérés!", projets);
-    // Création et insertion des éléments du DOM pour chaque projet
+
+    afficherProjets(projets); // Appel de la fonction pour afficher les projets
+}
+// Création et insertion des éléments du DOM pour chaque projet
+function afficherProjets(projets) {
      for (let i=0; i < projets.length; i++) {
         const ficheProjet = document.createElement("figure");
         const imageProjet = document.createElement("img");
@@ -19,27 +27,46 @@ async function recupererProjets() {
     }
 }
 
-recupererProjets();
+recupererProjets(); // Appel de la fonction pour récupérer et afficher les projets
+
 
 // Appel de l'API pour récupérer les catégories des filtres
 async function recupererCategories() {
     const response = await fetch("http://localhost:5678/api/categories")
-    const categories = await response.json();
+    categories = await response.json(); //liste des catégories
 
     console.log("Catégories récupérées!", categories);
-
-    // Création et insertion des boutons de filtre pour chaque catégorie
-    buttonTous = document.createElement("button");
+    
+    afficherCategories(categories); // Appel de la fonction pour afficher les catégories
+}
+// Création et insertion des boutons de filtre pour chaque catégorie
+function afficherCategories(categories) {
+    const buttonTous = document.createElement("button");
     buttonTous.innerText = "Tous";
     document.querySelector(".filters").appendChild(buttonTous);
 
     for (let i=0; i < categories.length; i++) {
-        const buttonFilter = document.createElement("button");
-        buttonFilter.innerText = categories[i].name;
-        document.querySelector(".filters").appendChild(buttonFilter);
+        const buttonsFilter = document.createElement("button");
+        buttonsFilter.innerText = categories[i].name;
+        buttonsFilter.value = categories[i].id;
+        document.querySelector(".filters").appendChild(buttonsFilter);
 
-        console.log("Bouton de filtre ajouté!", buttonFilter);
+        console.log("Bouton de filtre ajouté!", buttonsFilter);
     }
 }
 
-recupererCategories();
+recupererCategories(); // Appel de la fonction pour récupérer et afficher les boutons catégories
+
+// Ajout de la fonctionnalité de filtre aux boutons
+function filtrerProjets() {
+    const buttons = document.querySelectorAll(".filters button")
+
+    // Ajout d'event listener aux boutons
+    for (let i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function() {
+            const filtersProjets = projets.filter (function(projet) {
+                return projet.categoryId == buttonsFilter.value;
+            })
+        })
+    }
+}
