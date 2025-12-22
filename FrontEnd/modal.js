b719ffcf508566455c5ac5671a0f46006de1f50e
Marion Penel <marionpenel@hotmail.fr>
2025-12-22 12:58:43 +0100

    Gestion des focus dans la modale et suppression des focus sur la page principale lorsque la modale est ouverte

--- FrontEnd/modal.js
+++ FrontEnd/modal.js
@@ -2,16 +2,23 @@
 // Import de la fonction API
 import { getProjectsForModal } from './api.js';
 // Déclaration des constantes
+const main = document.querySelector("main")
 const modal = document.querySelector("#modal"); // Sélection de la modale
 const galleryModal = document.querySelector(".gallery-view"); // Sélection de la vue gallerie de la modale
 const formModal = document.querySelector(".form-view"); // Sélection de la vue formulaire de la modale
 const backButton = document.querySelector(".back-modal"); // Sélection du bouton retour de la modale
 
+let lastFocusedElement;
+
 // Fonction d'ouverture des modales
 function openModal (view) {
+    lastFocusedElement = document.activeElement;
+
     modal.style.display = "flex";
     modal.removeAttribute("aria-hidden");
     modal.setAttribute("aria-modal", "true");
+    modal.focus()
+    main.inert = true;
 
     if (view === "gallery") {
         galleryModal.style.display = "flex";
@@ -25,6 +32,7 @@ function openModal (view) {
         formModal.style.display = "flex";
         backButton.style.display = "flex";
     }
+    trapFocus(modal); // Appel de la fonction pour activer le focus trap à l'ouverture de la modale
 }
 
 // Gestion de l'affichage des travaux de la gallerie dans la modale
@@ -64,11 +72,33 @@ function backToGalleryView() {
     })
 }
 
+// Focus trap
+function trapFocus(modal){
+    const focusableElements = modal.querySelectorAll ("button, input, select");
+    const first = focusableElements[0];
+    const last = focusableElements[focusableElements.length - 1];
+
+    // Ecoute des touches Tab / Shift + Tab
+    modal.addEventListener("keydown", e => {
+        if (e.key !== "Tab") return;
+
+        if (e.shiftKey && document.activeElement === first) {
+            e.preventDefault();
+            last.focus();
+        } else if (!e.shiftKey && document.activeElement === last) {
+            e.preventDefault();
+            first.focus();
+        }
+    })
+}
+
 // Fermeture des modales
 function closeModal() {
     modal.style.display = "none";
     modal.setAttribute("aria-hidden", "true");
     modal.removeAttribute("aria-modal");
+    main.inert = false;
+    lastFocusedElement?.focus();
 }
 
 // Gestion de l'ouverture et de la fermeture des modales
@@ -89,18 +119,19 @@ export function initModal() {
     });
 
     backToGalleryView();
+
     //Ajout des écouteurs d'événements pour la fermeture de la modale
-    // Au clic sur la croix
+        // Au clic sur la croix
     closeModalButton.addEventListener("click", () => {
         closeModal()
     });
-    // Au clic en dehors du wrapper
+        // Au clic en dehors du wrapper
     window.addEventListener('click', (event) => {
         if (event.target === modal) { // Si on clique sur le fond noir (l'aside) et non le wrapper
             closeModal()
         }
     });
-    // A l'appuie sur la touche Echap/Escape/Esc
+        // A l'appuie sur la touche Echap/Escape/Esc
     window.addEventListener ("keydown", (event) => {
         if (event.key === "Escape" || event.key === "Esc") {
             closeModal()
