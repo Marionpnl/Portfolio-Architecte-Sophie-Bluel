fd6b1724e4d7ed8c7617e3e1aa8c4e68b4e4af57
Marion Penel <marionpenel@hotmail.fr>
2025-12-29 16:35:42 +0100

    Changement de couleur du bouton "Valider" lorsque tous les champs du formulaire sont remplis

--- FrontEnd/modal-form.js
+++ FrontEnd/modal-form.js
@@ -5,16 +5,57 @@ const form = document.querySelector(".form-photo");
 const fileInput = document.getElementById("file-input");
 const previewImage = document.querySelector(".photo-preview");
 const addPhotoDiv = document.querySelector(".add-photo");
+const submitBtn = document.querySelector(".submit-button");
 
+const title = document.querySelector('input[name="title"]');
+const category = document.querySelector('select[name="category"]');
+
+
+// Fonction pour la prévisualisation de l'image 
+function previewImageOnChange() {
+    fileInput.addEventListener ("change", () => {
+        const file = fileInput.files[0];
+        console.log(file);
+
+        const validTypes = ["image/jpeg", "image/png"];
+        if (!validTypes.includes(file.type)) {
+            alert ("Format invalide. JPG ou PNG uniquement");
+            fileInput.value = "";
+            changeButtonColorIfCompletedForm();
+            return;
+        }
+            
+        if (file.size > 4 * 1024 * 1024) {
+            alert("Image trop lourde (4Mo max)");
+            fileInput.value = "";
+            changeButtonColorIfCompletedForm();
+            return;
+        }
+
+        previewImage.src = URL.createObjectURL(file);
+        addPhotoDiv.classList.add("has-preview");
+
+        changeButtonColorIfCompletedForm();
+        });
+}
+
+// Fonction pour changer la couleur du bouton si le formulaire est correctement rempli
+function changeButtonColorIfCompletedForm () {
+    if (title.value && category.value && fileInput.files.length > 0) {
+        submitBtn.classList.add("active"); // bouton vert
+        submitBtn.disabled = false;
+    } else {
+        submitBtn.classList.remove("active"); // bouton gris
+        submitBtn.disabled = true;
+    }
+}
+
+// Fonction pour gérer l'envoi du formulaire au clic sur le bouton "Valider"
 function submittingAddForm () {
     form.addEventListener ("submit", async (event) => {
         event.preventDefault();
-
-        const image = document.querySelector("input[name='image']");
-        const title = document.querySelector('input[name="title"]');
-        const category = document.querySelector('select[name="category"]');
         
-        if (!image.files.length || !title.value || !category.value) {
+        if (!fileInput.files.length || !title.value || !category.value) {
             alert("Veuillez remplir tous les champs !");
             return;
         }
@@ -29,43 +70,28 @@ function submittingAddForm () {
 
         console.log("Listener submit actif");
         console.log("Projet ajouté", newProject);
-        
+        alert("Projet ajouté avec succès !");
+
         addProjectToGallery(newProject);
 
         form.reset();
+        previewImage.src = "";
+        addPhotoDiv.classList.remove("has-preview");
     })
 }
 
-function previewImageOnChange() {
-    fileInput.addEventListener ("change", () => {
-        const file = fileInput.files[0];
-        console.log(file);
-
-        const validTypes = ["image/jpeg", "image/png"];
-        if (!validTypes.includes(file.type)) {
-            alert ("Format invalide. JPG ou PNG uniquement");
-            fileInput.value = "";
-            return;
-        }
-            
-        if (file.size > 4 * 1024 * 1024) {
-            alert("Image trop lourde (4Mo max)");
-            fileInput.value = "";
-            return;
-        }
-
-        previewImage.src = URL.createObjectURL(file);
-
-        addPhotoDiv.classList.add("has-preview");
-        });
-}
-
+// 
 function initModalForm () {
     getCategoriesForModalForm();
     
     previewImageOnChange();
 
     submittingAddForm();
+
+    title.addEventListener("input", changeButtonColorIfCompletedForm);
+    category.addEventListener("change", changeButtonColorIfCompletedForm);
+    
+    changeButtonColorIfCompletedForm();
 }
 
 export { initModalForm };
