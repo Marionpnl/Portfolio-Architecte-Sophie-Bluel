f152427aa8f38c89b276949a63023278996e5429
Marion Penel <marionpenel@hotmail.fr>
2025-12-29 14:13:12 +0100

    Ajout de la preview

--- FrontEnd/modal-form.js
+++ FrontEnd/modal-form.js
@@ -2,6 +2,9 @@ import { addProject, getCategoriesForModalForm } from "./api.js";
 import { addProjectToGallery } from "./gallery.js";
 
 const form = document.querySelector(".form-photo");
+const fileInput = document.getElementById("file-input");
+const previewImage = document.querySelector(".photo-preview");
+const addPhotoDiv = document.querySelector(".add-photo");
 
 function submittingAddForm () {
     form.addEventListener ("submit", async (event) => {
@@ -19,14 +22,40 @@ function submittingAddForm () {
         console.log("Projet ajouté", newProject);
 
         addProjectToGallery(newProject);
-        
+
         form.reset();
     })
+}
+
+function previewImageOnChange() {
+    fileInput.addEventListener ("change", () => {
+        const file = fileInput.files[0];
+        console.log(file);
+        
+        const validTypes = ["image/jpeg", "image/png"];
+        if (!validTypes.includes(file.type)) {
+            alert ("Format invalide. JPG ou PNG uniquement");
+            fileInput.value = "";
+            return;
+        }
+            
+        if (file.size > 4 * 1024 * 1024) {
+            alert("Image trop lourde (4Mo max)");
+            fileInput.value = "";
+            return;
+        }
+
+        previewImage.src = URL.createObjectURL(file);
+
+        addPhotoDiv.classList.add("has-preview");
+        });
 }
 
 function initModalForm () {
     getCategoriesForModalForm();
     
+    previewImageOnChange();
+
     submittingAddForm();
 }
 
